429d1f3c982ffc5c5708b78d64937cb39b509844
Ourai Lin <ourairyu@gmail.com>
2022-09-07 16:41:49 +0800

    feat: add site

---
+++ site/_assets/javascripts/statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?536a029cb6f810c8b77eaecd38ecfac6";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
